683d4b5e7eea9bf50775c66cb8549aeb0ee30564
TATTARAM0X <jhonatan.ramox@gmail.com>
2022-10-09 15:47:10 -0700

    r

---
+++ r.py
@@ -0,0 +1,31 @@
+def hangman(attemps):
+    if attemps == 1:
+        print(" ( ) ")
+    if attemps == 2:
+        print(" ( ) ")
+        print("  |")
+    if attemps == 3:
+        print(" ( ) ")
+        print("/ |")
+    if attemps == 4:
+        print(" ( ) ")
+        print("/ | \\")
+    if attemps == 5:
+        print(" ( ) ")
+        print("/ | \\")
+        print("  |  ")
+    if attemps == 6:
+        print(" ( ) ")
+        print("/ | \\")
+        print("  |  ")
+        print(" /")
+    if attemps == 7:
+        print(" ( ) ")
+        print("/ | \\")
+        print("  |  ")
+        print(" / \\")
+    if attemps == 8:
+        print(" ( ) ")
+        print("/ | \\")
+        print("  |  ")
+        print(" /'\ ")
